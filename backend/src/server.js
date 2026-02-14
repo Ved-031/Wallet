0476e88a8357f858669aeaa9e3596e90bb44a4c0
Ved_031 <tellawarved@gmail.com>
2026-02-15 01:12:34 +0530

    added cron job to keep render project active

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import job from './config/cron.js';
 import { initDB } from './config/db.js';
 import rateLimiter from './middlewares/rateLimiter.js';
 import transactionRoutes from './routes/transaction.route.js';
@@ -8,11 +9,16 @@ import transactionRoutes from './routes/transaction.route.js';
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+if(process.env.NODE_ENV === 'production') job.start();
+
 app.use(cors());
 app.use(rateLimiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/api/health', (req, res) => {
+    res.status(200).json({ message: 'Healthy' });
+});
 app.use('/api/transactions', transactionRoutes);
 
 initDB().then(() => {
